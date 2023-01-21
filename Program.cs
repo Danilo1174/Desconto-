d65f9b9236fcf2c2ff9df8617f8457dcf8496e2a
Danilo Alves <daniloalves.1996@gmail.com>
2023-01-20 23:06:55 -0300

    Exercicio_desconto

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AtividadeDesconto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("infome o nome  produto: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("informe o preco do produto: ");
+            double preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("informe a quantidade que foram compradas: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Desconto produto = new Desconto(nome, preco, quantidade);
+
+
+            produto.MostarDesconto();
+
+
+        }
+    }
+}
---
+++ desconto.cs
@@ -0,0 +1,54 @@
+﻿namespace AtividadeDesconto
+{
+    using System;
+    class Desconto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        
+
+
+
+
+
+        public Desconto(string nome, double preco, int quantidade)
+        {
+            this.Nome = nome;
+            this.Preco = preco;
+            this.Quantidade = quantidade;
+
+
+
+        }
+        public double CalculoFinal() => this.Quantidade * this.Preco;
+
+        public double Total() => CalculoFinal();
+
+
+        public void MostarDesconto()
+        {
+
+
+            if (this.Quantidade >= 11 && this.Quantidade <= 20)
+            {
+                Console.WriteLine($"o valor a ser pago com desconto de 10%: {Total() - (0.1 * CalculoFinal())  }" );
+
+            }
+            if (this.Quantidade >= 21 && this.Quantidade <= 50)
+            {
+                Console.WriteLine($"o valor a ser pago com desconto de 20%: {Total() - (0.2 * CalculoFinal())} " );
+            }
+            if (this.Quantidade > 50)
+            {
+                Console.WriteLine($"o valor a ser pago com desconto de 25%: {Total() - (0.25 * CalculoFinal()) }" );
+                
+            }
+            if (this.Quantidade <= 10)
+            {
+              Console.WriteLine("o valor a ser pago é: " + Total());
+            }
+
+        }
+    }
+}
